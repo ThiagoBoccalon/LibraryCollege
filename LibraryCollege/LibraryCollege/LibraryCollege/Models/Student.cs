b9b185cf6675b2eae8c7f625981f827bc3d5b53f
ThiagoBoccalon <thiago_rodrigo90@hotmail.com>
2022-05-22 00:19:38 +0100

    Student Service to do.

---
+++ LibraryCollege/LibraryCollege/LibraryCollege/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LibraryCollege.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Int16 NumberStudent { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ LibraryCollege/LibraryCollege/LibraryCollege/Services/StudentService.cs
@@ -0,0 +1,22 @@
+﻿using LibraryCollege.Interfaces;
+using LibraryCollege.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryCollege.Services
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentService _studentService;
+        public StudentService(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        public void Insert(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
